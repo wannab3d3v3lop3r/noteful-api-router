9b5f35701f6e42c25ed1728d848c64c61c2a644d
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-09-16 14:08:05 -0400

    Updates

---
+++ precache-manifest.efa33a35fca63aeff32a9c8c8d03188a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefc097261e90e3fc36f8d21f03a6718",
+    "url": "/noteful-api-router/index.html"
+  },
+  {
+    "revision": "73de7a2e20d52d646ed4",
+    "url": "/noteful-api-router/static/css/main.cd244bcc.chunk.css"
+  },
+  {
+    "revision": "c0d0f6f39fdef62820fe",
+    "url": "/noteful-api-router/static/js/2.e7103a9c.chunk.js"
+  },
+  {
+    "revision": "73de7a2e20d52d646ed4",
+    "url": "/noteful-api-router/static/js/main.0be7c3f2.chunk.js"
+  },
+  {
+    "revision": "e920db98f1bae4987dd3",
+    "url": "/noteful-api-router/static/js/runtime~main.a692243c.js"
+  }
+]);
